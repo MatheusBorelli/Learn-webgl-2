db0981640860d7d5466567a3a7bcb095d147a9c9
MatheusBorelli <73211294+MatheusBorelli@users.noreply.github.com>
2022-06-06 22:44:16 -0300

    Removed Comments

--- assets/shaders/VertexShader.glsl
+++ assets/shaders/VertexShader.glsl
@@ -4,43 +4,10 @@
 // It will receive data from a buffer
 in vec2 a_position;
 
-//uniform vec2 u_resolution;
-
 uniform mat3 u_matrix;
-//uniform vec2 u_translation;
-//uniform vec2 u_rotation;
-//uniform vec2 u_scale;
 
 // all shaders have a main function
-void main() { 
-    /* Old Version of Translation , Rotation and Scaling
-    
-    //Scale the position
-    vec2 scaledPosition = a_position * u_scale;
-
-    //Rotate the position
-    vec2 rotatedPosition = vec2(
-        scaledPosition.x * u_rotation.y + scaledPosition.y * u_rotation.x,
-        scaledPosition.y * u_rotation.y - scaledPosition.x * u_rotation.x);
-
-    //Add in the translation
-    vec2 position = rotatedPosition + u_translation;
-    */
-    
-    /*  Old method to fix bottom left to (0,0) and change position to pixels
-
-    vec2 position = (u_matrix * vec3(a_position, 1)).xy;
-    
-    convert position to pixels (max height in pixel resolution(720px) = 1.0 in y axis in webGL)
-    vec2 resolutionGrid = position / u_resolution;
-
-   vec2 zeroToTwo = resolutionGrid * 2.0;
-    vec2 clipSpace = zeroToTwo - 1.0;
-    makes pixels (0,0) the bottom left position
-    resolutionGrid *= 2.0;
-    resolutionGrid -= 1.0;
-    */
-
+void main() {
     // gl_Position is a special variable a vertex shader
     // is responsible for setting
     gl_Position = vec4(( u_matrix * vec3(a_position, 1)).xy , 0 , 1);
